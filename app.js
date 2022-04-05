3e3a374e2865af23b14e8219a173a8ef78ae80fa
refresh <fly2nd@gmail.com>
2022-04-05 17:59:47 +0900

    login complete

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+function paintGreetings(username) {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
-  event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
     event.preventDefault();
-    console.log(event);
-    alert("clicked!");
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
