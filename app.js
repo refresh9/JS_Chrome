8eff1b7bf747645b59c71cfe5f2650349456e719
refresh <fly2nd@gmail.com>
2022-04-05 17:25:05 +0900

    login-form

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-loginButton.addEventListener("click", function(event) {
-    const username = loginInput.value;
-    if (username === "") {
-        alert("Please wrtie your name.");
-    } else if (username.length > 15) {
-        alert("Your name is too long.")
-    }
-});
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
