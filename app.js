51b7823b85497bbcaca622df9612fb3dc909246a
refresh <fly2nd@gmail.com>
2022-04-05 11:32:48 +0900

    event added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const title = document.querySelector('div.hello:first-child h1');
-title.addEventListener('click', function() {
-    title.style.color = 'blue';
+const h1 = document.querySelector('div.hello:first-child h1');
+h1.addEventListener('click', function() {
+    h1.style.color = 'blue';
 });
-title.addEventListener('mouseenter', function() {
-    title.innerText = 'Mouse is here!';
+h1.addEventListener('mouseenter', function() {
+    h1.innerText = 'Mouse is here!';
 });
-title.addEventListener('mouseleave', function() {
-    title.innerText = 'Mouse is gone!';
+h1.addEventListener('mouseleave', function() {
+    h1.innerText = 'Mouse is gone!';
+});
+window.addEventListener('resize', function() {
+    this.document.body.style.backgroundColor = 'tomato';
+});
+window.addEventListener('copy', function() {
+    alert('copier!');
 });
