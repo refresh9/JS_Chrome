4a1f51bf1f8f48b87b731dd1551de0167805b00c
refresh <fly2nd@gmail.com>
2022-04-05 11:23:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const title = document.querySelector('div.hello:first-child h1');
 title.addEventListener('click', function() {
-    console.log('title was clicked!');
+    title.style.color = 'blue';
+});
+title.addEventListener('mouseenter', function() {
+    title.innerText = 'Mouse is here!';
+});
+title.addEventListener('mouseleave', function() {
+    title.innerText = 'Mouse is gone!';
 });
